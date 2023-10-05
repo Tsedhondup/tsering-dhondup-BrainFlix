d0a940d3dfcacecb536ffe80be1adca86df0be4c
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-04 23:14:05 -0400

    video-data was imported

--- src/components/CurrentVid/CurrentVid.js
+++ src/components/CurrentVid/CurrentVid.js
@@ -1,3 +1,6 @@
 import "./CurrentVid.scss";
-let CurrentVid = () => {};
+import data from "../../data/video-details.json";
+import { useState } from "react";
+let CurrentVid = () => {
+console.log(data)};
 export default CurrentVid;
