182a1af497794b19e52c8ca589944e602ce459a1
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-05 20:43:58 -0400

    className for header logo-container was upated

--- src/components/Header/HeaderUpload/HeaderUpload.js
+++ src/components/Header/HeaderUpload/HeaderUpload.js
@@ -1,8 +1,8 @@
 import "./HeaderUpload.scss";
 let HeaderUpload = () => {
   return (
-    <div className="header__upload-btn-container">
-      <button className="header__upload-btn-container--button">upload</button>
+    <div className="header-upload-btn-container">
+      <button className="header-pload-btn-container--button">upload</button>
     </div>
   );
 };
