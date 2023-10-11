10f6d9bbc71a3efb04899de55611a8e8c8984e10
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-10 21:17:07 -0400

    style for search and mobile-profile for screen min-width 320px were added

--- src/components/Header/HeaderProfileMobile/HeaderProfileMobile.js
+++ src/components/Header/HeaderProfileMobile/HeaderProfileMobile.js
@@ -2,13 +2,26 @@ import "./HeaderProfileMobile.scss";
 import Profile from "../../../assets/images/Mohan-muruge.jpg";
 let HeaderProfileMobile = () => {
   return (
+    //  PROFILE-IMAGE
     <div className="header-mobile-profile-container">
-      <img
-        className="header-mobile-profile-container__profile"
-        src={Profile}
-        alt="profile"
-      ></img>
+      {/*  IMAGE-WRAPPER WAS CREATED TO MAKE LANDSCAPE IMAGE CICULAR  */}
+      <div className="header-mobile-profile-container__img-wrapper">
+        {/*  PROFILE-IMAGE */}
+        <img
+          className="header-mobile-profile-container__img-wrapper--img"
+          src={Profile}
+          alt="profile"
+        />
+      </div>
     </div>
   );
 };
 export default HeaderProfileMobile;
+// {/*---------------------------------------------------------*/}
+//   <div className="header-mobile-profile-container">
+//     <img
+//       className="header-mobile-profile-container__profile"
+//       src={Profile}
+//       alt="profile"
+//     ></img>
+//   </div>
