8ceeddaec4e5fc0fed7ba6ac28cc7561756dfa73
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-11 19:54:34 -0400

    temporary styles were added

--- src/components/NextVid/NextVid.js
+++ src/components/NextVid/NextVid.js
@@ -4,6 +4,11 @@ import NextVidTitle from "./NextVidTitle/NextVidTitle";
 import NextVidImage from "./NextVidImagel/NextVidImage";
 
 let NextVid = (props) => {
+  
+  // STATE-VIDEO HANDLER
+  const setVideoHandler = () => {
+
+  };
   // CREATING THE NEXT-VDIEO LIST
   const list = props.nextVideos.map((vidObj) => (
     // NEXT-VIDEO-ITEM
