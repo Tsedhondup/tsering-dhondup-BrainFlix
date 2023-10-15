ae267a109448d0839ba55d33d72bf7cca52f354a
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-15 16:23:39 -0400

    media-query was applied to HeaderMobileProfile and HeaderTabletProfile component

--- src/components/Header/HeaderProfileTablet/HeaderProfileTablet.js
+++ src/components/Header/HeaderProfileTablet/HeaderProfileTablet.js
@@ -2,13 +2,26 @@ import "./HeaderProfileTablet.scss";
 import Profile from "../../../assets/images/Mohan-muruge.jpg";
 let HeaderProfileTablet = () => {
   return (
+    //  PROFILE-IMAGE
     <div className="header-tablet-profile-container">
-      <img
-        className="header-tablet-profile-container__profile"
-        src={Profile}
-        alt="profile"
-      ></img>
+      {/*  IMAGE-WRAPPER WAS CREATED TO MAKE LANDSCAPE IMAGE CICULAR  */}
+      <div className="header-tablet-profile-container__img-wrapper">
+        {/*  PROFILE-IMAGE */}
+        <img
+          className="header-tablet-profile-container__img-wrapper--img"
+          src={Profile}
+          alt="profile"
+        />
+      </div>
     </div>
+    //---------------------------------------------//
+    // <div className="header-tablet-profile-container">
+    //   <img
+    //     className="header-tablet-profile-container__profile"
+    //     src={Profile}
+    //     alt="profile"
+    //   ></img>
+    // </div>
   );
 };
 
