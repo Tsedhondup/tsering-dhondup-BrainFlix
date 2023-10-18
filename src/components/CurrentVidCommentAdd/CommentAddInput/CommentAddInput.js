d7f948b2cd1c4cf51e5ce16767a6e3d42de39bb9
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-17 23:27:29 -0400

    aspect-ratio of 16:9 was set to current video
    comment-adding input for screen min-width:768px was added

--- src/components/CurrentVidCommentAdd/CommentAddInput/CommentAddInput.js
+++ src/components/CurrentVidCommentAdd/CommentAddInput/CommentAddInput.js
@@ -2,12 +2,20 @@ import "./CommentAddInput.scss";
 let CommentAddInput = () => {
   return (
     <div className="current-vid-input-and-btn-container__input-container">
+      {/* INPUT FOR SCREEN MIN-WIDTH:320PX / MOBILE*/}
+
       <textarea
-        className="current-vid-input-and-btn-container__input-container--input"
+        className="current-vid-input-and-btn-container__input-container--input-mobile"
         id="comment-input"
         type="text"
         placeholder="Add a new comment"
       />
+      {/* INPUT FOR SCREEN MIN-WIDTH:768PX AND BEYOND  */}
+      <input
+        className="current-vid-input-and-btn-container__input-container--input-tablet"
+        type="text"
+        placeholder="Add a new comment"
+      ></input>
     </div>
   );
 };
--- src/components/CurrentVideo/CurrentVideo.js
+++ src/components/CurrentVideo/CurrentVideo.js
@@ -4,13 +4,44 @@ import "./CurrentVideo.scss";
 let CurrentVideo = (props) => {
   // RENDERING
   return (
-    <div className="video-section__current-video-container">
-      <video
-        id={props.currentVideo.id}
-        className="video-section__current-video-container--video"
-        poster={props.currentVideo.image}
-        controls
-      ></video>
+    /* 
+    # WRAPPER LEVEL-2 : PURPOSES
+    *
+    * TO HAVE 100% WIDTH RELATIVE TO VIEW-PORT
+    * TO CONTAIN WRAPPER-LEVEL-1
+    * HAS BACKGROUND OF 'BRAIN-FLIX-DARK-BLACK' 
+    */
+    <div className="current-video-wrapper-level-2">
+      {/* 
+    # WRAPPER LEVEL-1 : PURPOSES
+    *
+    * TO APPLIED APPROPRIATE WIDTH CURRENT-VIDEO SECTION TO MATCH THE MOCK UP
+    * TO APPLIED PADDING LEFT AND RIGHT FOR SCREEN MIN-WIDTH:768PX
+    */}
+      <div className="current-video-wrapper-level-1">
+        {/* 
+      # WRAPPER-LEVEL-0 : PURPOSES 
+      *
+      * TO CONTAIN VIDEO-ELEMENT
+      * HAVE ASPECT RATIO OF 16:9 === WIDTH OF 16 UNITS & HEIGHT OF 9 UNITS 
+      * TO CALCULATE HEIGHT FOR ASPECT RATIO 16:9 = 9/16 === 0.5625 / 56.25%
+      *
+      * CURRENT-VIDEO-WRAPPER-LEVEL-0 HAS HEIGHT OF '0 / 0%'
+      * INSTEAD IT HAS PADDING-TOP OF 56.25% === 56.25% OF ITS TOTAL WIDTH WHICH IS SET TO 100%;
+      * 56.25% PADDING-TOP ADDING TO ITS HEIGHT === MAKES TOTAL HEIGHT OF 56.25%
+      */}
+        <div className="current-video-wrapper-level-0">
+          {/*
+           * VIDEO HAS ITS BOTH HEIGHT AND WIDHT SET TO 100% OF ITS CONTAINER === WRAPPER-LEVEL-0
+           */}
+          <video
+            id={props.currentVideo.id}
+            className="current-video-wrapper-level-0__video"
+            poster={props.currentVideo.image}
+            controls
+          ></video>
+        </div>
+      </div>
     </div>
   );
 };
