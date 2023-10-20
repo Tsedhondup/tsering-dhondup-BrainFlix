b8760e84dc4379ad89c967b42b1b0d8995a6ad25
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-20 19:23:03 -0400

    comments for current-video component was updated

--- src/components/CurrentVideo/CurrentVideo.js
+++ src/components/CurrentVideo/CurrentVideo.js
@@ -1,22 +1,32 @@
 import "./CurrentVideo.scss";
 
-// HANDLE CURRENT VIDEO
 let CurrentVideo = (props) => {
-  // RENDERING
+  /*
+   * THREE LAYERS OF WRAPPERS FOR FOR CURRENT-VIDEO/VIDEO TAG
+   *
+   *    ...< WRAPPER-LEVEL-2 />
+   *      ...< WRAPPER-LEVEL-1 />
+   *        ...< WRAPPER-LEVEL-0 />
+   *             < VIDEO-TAG* />
+   *        ...< WRAPPER-LEVEL-0 />
+   *      ...< WRAPPER-LEVEL-1 />
+   *    ...< WRAPPER-LEVEL-2 />
+   */
   return (
     /* 
     # WRAPPER LEVEL-2 : PURPOSES
     *
-    * TO HAVE 100% WIDTH RELATIVE TO VIEW-PORT
-    * TO CONTAIN WRAPPER-LEVEL-1
+    * HAS WIDTH OF 100% OF VIEW-PORT
     * HAS BACKGROUND OF 'BRAIN-FLIX-DARK-BLACK' 
+    * CONTAIN WRAPPER-LEVEL-1
     */
     <div className="current-video-wrapper-level-2">
       {/* 
     # WRAPPER LEVEL-1 : PURPOSES
     *
-    * TO APPLIED APPROPRIATE WIDTH CURRENT-VIDEO SECTION TO MATCH THE MOCK UP
-    * TO APPLIED PADDING LEFT AND RIGHT FOR SCREEN MIN-WIDTH:768PX
+    * TO APPLIED APPROPRIATE WIDTH TO CURRENT-VIDEO SECTION TO MATCH THE MOCK UP
+    * TO APPLIED PADDING LEFT AND RIGHT FOR SCREEN MIN-WIDTH:768PX & BEYOND
+    * CONTAIN WRAPPER-LEVEL-0
     */}
       <div className="current-video-wrapper-level-1">
         {/* 
@@ -24,15 +34,19 @@ let CurrentVideo = (props) => {
       *
       * TO CONTAIN VIDEO-ELEMENT
       * HAVE ASPECT RATIO OF 16:9 === WIDTH OF 16 UNITS & HEIGHT OF 9 UNITS 
-      * TO CALCULATE HEIGHT FOR ASPECT RATIO 16:9 = 9/16 === 0.5625 / 56.25%
+      * TO CALCULATE HEIGHT OF BOX [] FOR ASPECT RATIO OF 16:9 = 
+      * HEIGHT = HEIGHT/WIDTH = 9/16 === 0.5625 OR 56.25%
       *
-      * CURRENT-VIDEO-WRAPPER-LEVEL-0 HAS HEIGHT OF '0 / 0%'
-      * INSTEAD IT HAS PADDING-TOP OF 56.25% === 56.25% OF ITS TOTAL WIDTH WHICH IS SET TO 100%;
+      * SETTING HEIGHT OF CURRENT-WRAPPER-LEVEL-0
+      * SET HEIGHT OF '0%'
+      * SET PADDING-TOP OF 56.25% === 56.25% OF ITS TOTAL WIDTH WHICH IS SET TO 100%;
       * 56.25% PADDING-TOP ADDING TO ITS HEIGHT === MAKES TOTAL HEIGHT OF 56.25%
       */}
         <div className="current-video-wrapper-level-0">
           {/*
-           * VIDEO HAS ITS BOTH HEIGHT AND WIDHT SET TO 100% OF ITS CONTAINER === WRAPPER-LEVEL-0
+             VIDEO HAS ITS BOTH WIDTH AND HEIGHT SET TO 100% OF ITS CONTAINER(WRAPPER-LEVEL-0)
+             WHICH HAS AN ASPECT RATIO OF 16:9*
+            
            */}
           <video
             id={props.currentVideo.id}
