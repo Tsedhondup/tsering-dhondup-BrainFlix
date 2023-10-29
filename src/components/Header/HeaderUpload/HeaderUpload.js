909ae23c4bfdc4276dfa81f3e90c1c7ee3562eee
Tsering Dhondup <tyddhondup88@gmail.com>
2023-10-29 11:46:45 -0400

    nav link was imported

--- src/components/Header/HeaderUpload/HeaderUpload.js
+++ src/components/Header/HeaderUpload/HeaderUpload.js
@@ -1,10 +1,15 @@
+// REACT-ROUTER-DOM
+import { NavLink } from "react-router-dom";
 // STYLE
 import "./HeaderUpload.scss";
 // ASSET
 import uploadIcon from "../../../assets/images/upload.svg";
+// COMPONENTS
+// import VideoUploadPage from "../../../Page/VideoUploadPage/VideoUploadPage";
 
 const HeaderUpload = () => {
   return (
+    // <Link to={"/videoUploadPage"}>
     <div className="header-upload-btn-container">
       {/* UPLOAD BUTTON */}
       <button className="header-upload-btn-container__button">
